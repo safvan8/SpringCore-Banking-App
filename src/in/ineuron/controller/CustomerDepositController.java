cbdc68ec50e2976cedb5c9d2e285bd4da2f78e37
Safvan <safvanpmcz@gmail.com>
2023-03-14 13:29:57 +0530

    Application dependency injection accross multiple layers are successfull

--- src/in/ineuron/controller/CustomerDepositController.java
+++ src/in/ineuron/controller/CustomerDepositController.java
@@ -16,17 +16,19 @@ public class CustomerDepositController
 
 	public CustomerDepositController(ICustomerDepositService depositService)
 	{
+		this.depositService = depositService;
 		System.out.println("CustomerController One arg constrtur :: is called");
 		System.out.println("depositServiceImpl object injected to Controller");
 	}
-	
+
 	/**
 	 * 
-	 * @param depositVO The Value object containing customer deposit details in String.
+	 * @param depositVO The Value object containing customer deposit details in
+	 *                  String.
 	 */
 	public void peformDepsoit(CustomerDepositVO depositVO)
 	{
 
 	}
-	
+
 }
--- src/in/ineuron/main/ClientApp.java
+++ src/in/ineuron/main/ClientApp.java
@@ -1,11 +1,45 @@
 package in.ineuron.main;
 
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import in.ineuron.controller.CustomerDepositController;
 import in.ineuron.vo.CustomerDepositVO;
 
 public class ClientApp
 {
+	private static DefaultListableBeanFactory beanFactory = null;
+	private static XmlBeanDefinitionReader reader = null;
+
+	private static CustomerDepositController controller = null;
+
+	static
+	{
+		try
+		{
+			DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+			XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+			int numberOFBeanDefinitionsLoaded = reader.loadBeanDefinitions("/in/ineuron/resources/applicationContext.xml");
+			controller = beanFactory.getBean("controller", CustomerDepositController.class);
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new ExceptionInInitializerError();
+			
+		}
+		finally {
+			System.out.println("__________________________________________________________"
+					+ "____________________________________________________________"
+					+ "____________________________________________________________"
+					+ "____________________________________________");
+		}
+
+	}
+
 	public static void main(String[] args)
 	{
+
 		// Conatins methods for all type of user inputs
 		CustomerInputHandler inputHandler = CustomerInputHandler.getCustomerInputHandler();
 
@@ -14,5 +48,9 @@ public class ClientApp
 
 		// to get all customer details from the user in the form of Value object
 		CustomerDepositVO customerDepositVO = inputHandler.acceptDepositInfoFromCustomer();
+		
+//		// passing customerDeposit Vo to controller;
+//		controller.
+
 	}
 }
